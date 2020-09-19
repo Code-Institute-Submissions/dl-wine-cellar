2942d79e18269e6f25ce4036450bbd9b7adc0401
LawlessXD <DavidLawless@gmail.com>
2020-09-19 16:22:07 +0100

    Setup allauth with Google

--- wine_cellar/settings.py
+++ wine_cellar/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    #allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google', # Allow users to use their Google account to sign up
 ]
 
 MIDDLEWARE = [
@@ -60,12 +67,44 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.auth.context_processors.auth', # Required for allauth
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # This outputs email to the console which we will use temporaily
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email' # Allows user to use username or email to login
+ACCOUNT_EMAIL_REQUIRED = True # Email is required to register for the site
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory' # Ask users to verify their email address so we know they are using a real email address
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True # 
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/' # redirects user to the home page upon successful login
 
 WSGI_APPLICATION = 'wine_cellar.wsgi.application'
 
