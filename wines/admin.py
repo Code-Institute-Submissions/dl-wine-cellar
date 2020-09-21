b7f04af2d6496878b6a9de25b207b07f92f7fbdf
LawlessXD <DavidLawless@gmail.com>
2020-09-21 14:18:20 +0100

    Customised Admin

--- wines/admin.py
+++ wines/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 from .models import Wine, Category, Grape
 
 # Register models here
-admin.site.register(Wine)
-admin.site.register(Category)
-admin.site.register(Grape)
+
+class WineAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_code',
+        'name',
+        'country',
+        'price',
+        'category',
+        'rating',
+        'image',
+    )
+
+    ordering = ('product_code',)
+
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class GrapeAdmin(admin.ModelAdmin):
+        list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Grape, GrapeAdmin)
