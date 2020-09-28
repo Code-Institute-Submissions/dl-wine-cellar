1e381e37f572b439538f65c4ef6d64e8f50b7125
LawlessXD <DavidLawless@gmail.com>
2020-09-28 15:33:05 +0100

    Addded checkout success logic

--- wine_cellar/settings.py
+++ wine_cellar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['2cea44d5229f.ngrok.io']
+ALLOWED_HOSTS = ['9d21ecf3a23c.ngrok.io']
 
 
 # Application definition
