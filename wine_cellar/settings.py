d73c05b121ee22c5f9414127d4861edaba756226
LawlessXD <DavidLawless@gmail.com>
2020-09-29 22:07:18 +0100

    Updated allauth templates and tested profiles

--- wine_cellar/settings.py
+++ wine_cellar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['00e561e67bb5.ngrok.io']
+ALLOWED_HOSTS = ['00e561e67bb5.ngrok.io','127.0.0.1']
 
 
 # Application definition
